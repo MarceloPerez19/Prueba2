45ae967ea8c2c7a422dc6852100c6b36c3309851
user <user@USER-PC>
2020-11-08 14:49:52 -0500

    Agregar archivos de proyecto.

---
+++ Prueba2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Esta es una prueba para manual GIT");
+            Console.ReadKey();
+        }
+    }
+}
