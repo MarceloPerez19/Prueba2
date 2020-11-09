f8363c54eda04afebd42bc643ec0661063ada2d1
user <user@USER-PC>
2020-11-09 09:26:45 -0500

    funcion suma de dos numeros

--- Prueba2/Program.cs
+++ Prueba2/Program.cs
@@ -13,6 +13,27 @@ namespace Prueba2
             Console.WriteLine("Hola mundo");
             Console.WriteLine("Esta es una prueba para manual GIT");
             Console.ReadKey();
+
+
+            //Saludos desde la PUCESE
+            Console.WriteLine("BIENVENIDO A LA PUCESE");
+            Console.WriteLine("SEGUNDO DE TIC");
+
+            //Fin de clase
+            Console.WriteLine("Time is over...");
+
+            Console.WriteLine("Probando la funcion");
+            int suma = funcionSuma(10, 20);
+            Console.WriteLine("Resultado funcion = {0}", suma);
+
+            Console.ReadKey();
+        }
+
+        //funcion suma
+        public static int funcionSuma(int a, int b)
+        {
+            return (a * 10 + b * 100);
+
         }
     }
 }
