a8a79be8438833283f4f7e3ade8330af2cc5b221
user <user@USER-PC>
2020-11-09 09:34:36 -0500

    cambio de funcion

--- Prueba2/Program.cs
+++ Prueba2/Program.cs
@@ -12,7 +12,28 @@ namespace Prueba2
         {
             Console.WriteLine("Hola mundo");
             Console.WriteLine("Esta es una prueba para manual GIT");
+            
+
+
+            //Saludos desde la PUCESE
+            Console.WriteLine("BIENVENIDO A LA PUCESE");
+            Console.WriteLine("SEGUNDO DE TIC");
+
+            //Fin de clase
+            Console.WriteLine("Time is over...");
+
+            Console.WriteLine("Probando la funcion");
+            Console.WriteLine("Resultado de la suma de 10 + 20 es {0}", funcionSuma(10, 20);
+
             Console.ReadKey();
         }
+
+         public static funcionSuma(int a, int b)
+        {
+            return (a + b); 
+        }
+
+
+        
     }
 }
